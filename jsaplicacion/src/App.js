e546d180775b0c19d79330ba726865710c882c7c
Jesus Seiler <ichbinseiler@gmail.com>
2023-11-09 11:38:28 -0300

    esqueleto de la primera parte

--- jsaplicacion/src/App.js
+++ jsaplicacion/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [imageUrl, setImageUrl] = useState('');
+
+  const handleImageAnalysis = () => {
+    // Lógica para el análisis de imágenes (se agregará en futuras fases)
+    console.log('Análisis de imágenes desencadenado');
+  };
+
+  const handleImageGeneration = () => {
+    // Lógica para la generación de imágenes (se agregará en futuras fases)
+    console.log('Generación de imágenes desencadenada');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Bienvenido a la aplicación de prueba de Jesus Seiler
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Aplicación de Análisis de Imágenes</h1>
+      <label htmlFor="imageUrl">URL de la imagen:</label>
+      <input
+        type="text"
+        id="imageUrl"
+        value={imageUrl}
+        onChange={(e) => setImageUrl(e.target.value)}
+      />
+      <br />
+      <button onClick={handleImageAnalysis}>Analizar Imagen</button>
+      <button onClick={handleImageGeneration}>Generar Imagen</button>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
